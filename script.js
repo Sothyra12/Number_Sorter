499b1d30c4ca8984baaec88ad414822d6f06288e
Sothyra Chan <schan256@my.centennialcollege.ca>
2024-11-27 22:25:46 -0500

    added functions and variables to render the dropdown values in the html output

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // script.js
+
+const sortButton = document.getElementById("sort");
+
+const sortInputArray = (event) => {
+  // prevent the behavior of the form as buttons associated with the form by default
+  event.preventDefault();
+
+  const inputValues = [
+    ...document.getElementsByClassName("values-dropdown"),
+  ].map((dropdown) => Number(dropdown.value));
+
+  updateUI(inputValues);
+};
+
+const updateUI = (array = []) => {
+  array.forEach((num, i) => {
+    const outputValueNode = document.getElementById(`output-value-${i}`);
+    outputValueNode.innerText = num;
+  });
+};
+
+sortButton.addEventListener("click", sortInputArray);
