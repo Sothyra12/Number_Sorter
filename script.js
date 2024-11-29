bf2b26d3f7d7021090c3bb693f0e39bf6ed9d5ed
Sothyra Chan <schan256@my.centennialcollege.ca>
2024-11-28 23:49:33 -0500

    created a bubble sort function to bubble sort the array elements

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ const sortInputArray = (event) => {
     ...document.getElementsByClassName("values-dropdown"),
   ].map((dropdown) => Number(dropdown.value));
 
-  updateUI(inputValues);
+  const sortedValues = bubbleSort(inputValues);
+  updateUI(sortedValues);
 };
 
 const updateUI = (array = []) => {
@@ -18,6 +19,28 @@ const updateUI = (array = []) => {
     const outputValueNode = document.getElementById(`output-value-${i}`);
     outputValueNode.innerText = num;
   });
+};
+
+// Implement bubble sort algorithm
+// bubble sort is a simple sorting algorithm that repeatedly steps through the list, compares adjacent elements and swaps them if they are in the wrong order
+const bubbleSort = (array) => {
+  // use for loop to iterate through the array for simplicity
+  for (let i = 0; i < array.length; i++) {
+    // use another for loop to iterate through the array except the last element
+    for (let j = 0; j < array.length - 1; j++) {
+      // compare the current element with the next element to check if they are in the wrong order and swap them meaning to bubble sort
+      if (array[j] > array[j + 1]) {
+        if (array[j] > array[j + 1]) {
+          const temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+        }
+      }
+    }
+  }
+
+  // return the sorted array after the outer loop is done
+  return array;
 };
 
 sortButton.addEventListener("click", sortInputArray);
