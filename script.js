fe099b5c3f37f716ce5d248c7f253e866ac00385
Sothyra Chan <schan256@my.centennialcollege.ca>
2024-11-29 00:07:10 -0500

    created a selection sort function to select the smallest value and swap it with the first index

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ const sortInputArray = (event) => {
     ...document.getElementsByClassName("values-dropdown"),
   ].map((dropdown) => Number(dropdown.value));
 
-  const sortedValues = bubbleSort(inputValues);
+  // const sortedValues = bubbleSort(inputValues);
+  const sortedValues = selectionSort(inputValues);
   updateUI(sortedValues);
 };
 
@@ -21,7 +22,6 @@ const updateUI = (array = []) => {
   });
 };
 
-// Implement bubble sort algorithm
 // bubble sort is a simple sorting algorithm that repeatedly steps through the list, compares adjacent elements and swaps them if they are in the wrong order
 const bubbleSort = (array) => {
   // use for loop to iterate through the array for simplicity
@@ -41,6 +41,20 @@ const bubbleSort = (array) => {
 
   // return the sorted array after the outer loop is done
   return array;
+};
+
+// Selection sort works by repeatedly selecting the smallest element from the unsorted portion of the array and placing it at the beginning of the unsorted portion of the array
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    // ensures if the current element is the smallest element, it will be swapped with the itself not be moved
+    const minIndex = i;
+    for (let j = i + 1; j < array.length; j++) {
+      console.log(array, array[j], array[minIndex]);
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+  }
 };
 
 sortButton.addEventListener("click", sortInputArray);
